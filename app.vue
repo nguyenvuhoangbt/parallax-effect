cf3c84cccd9e02cbdc66759b9f2c8a136b15135b
nguyenvuhoangbt <55265803+nguyenvuhoangbt@users.noreply.github.com>
2024-07-20 23:47:53 +0700

    fix: build failed

--- app.vue
+++ app.vue
@@ -11,7 +11,7 @@ useHead({
       <main class="h-screen">
         <Layers />
 
-        <div class="h-screen bg-slate-400 relative">
+        <div class="h-screen bg-slate-400 relative z-10">
           <h1 class="text-center text-7xl p-4">Content here</h1>
         </div>
       </main>
